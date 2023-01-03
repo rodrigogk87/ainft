a2bfebd00210f9ade8a516ebc7720736d0be059a
rodrigogk87 <rodrigogk87@gmail.com>
2023-01-03 20:02:44 -0300

    Update Ainft.sol

--- blockchain/contracts/Ainft.sol
+++ blockchain/contracts/Ainft.sol
@@ -1,74 +1,39 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
-import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract AiNFT is ERC721A, Ownable {
-    using Strings for uint256;
-    
-    string public baseURI; 
-    string public baseExtension = ".json";
-    uint256 public MAX_AMOUNT_PER_MINT = 10;
-    uint256 public COST_PER_NFT = 0.08 ether;
-    uint256 public maxNFT = 555;
-    bool metadataLocked = false;
+contract AINFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint256 public price = 0.005 ether;
 
-    constructor() ERC721A("Ai Nft", "AINFT") {}
+    constructor() ERC721("AINFTs", "AINFT") {}
 
-    function mint(uint256 quantity) external payable {
-        require(_totalMinted() + quantity <= maxNFT,"not enough NFT to mint");
-        require(msg.value >= quantity * COST_PER_NFT, "not enough ethers sent");
-        require(quantity <= MAX_AMOUNT_PER_MINT,"cant mint more than max amount per mint");
-        _safeMint(msg.sender, quantity);
+    function mint() public payable returns (uint256 newItemId_) {
+        require(msg.value >= price, "not enough eth sent");
+        _tokenIds.increment();
+
+        newItemId_ = _tokenIds.current();
+        _mint(msg.sender, newItemId_);
     }
 
-    function totalMinted() public view returns(uint256){
-        return _totalMinted();
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
+    /**
+     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function setTokenURI(uint256 _tokenId, string memory _tokenURI)
+        internal
         virtual
-        override(ERC721A)
-        returns (string memory)
+        onlyOwner
     {
-        require(
-        _exists(tokenId),
-        "URI query for nonexistent token!"
-        );
-
-        string memory currentBaseURI = _baseURI();
-        return bytes(currentBaseURI).length > 0
-            ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), ".json"))
-            : "";
+        _setTokenURI(_tokenId, _tokenURI);
     }
-
-    function _baseURI() internal view virtual override(ERC721A) returns (string memory) {
-        return baseURI;
-    }
-
-    function setCost(uint256 _newCost) public onlyOwner {
-        COST_PER_NFT = _newCost;
-    }
-
-    function setBaseURI(string memory _newBaseURI) public onlyOwner {
-        require(metadataLocked == false, "Metadata locked");
-        baseURI = _newBaseURI;
-    }
-
-    function lockMetadata() public onlyOwner {
-        require(metadataLocked == false, "Metadata already locked");
-        metadataLocked = true;
-    }
-
-    function withdraw() public onlyOwner {
-        uint256 balance = address(this).balance;
-        payable(msg.sender).transfer(balance);
-    }
-
-    //receive function
-    receive() external payable {}
 }
