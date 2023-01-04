b5e5faec96d168855b58a1f1d878487987379827
rodrigogk87 <rodrigogk87@gmail.com>
2023-01-04 18:49:37 -0300

    Update Ainft.sol

--- blockchain/contracts/Ainft.sol
+++ blockchain/contracts/Ainft.sol
@@ -1,31 +1,97 @@
-//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract AINFT is ERC721URIStorage, Ownable {
+contract AINFT is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    Pausable,
+    Ownable,
+    ERC721Burnable
+{
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+
+    Counters.Counter private _tokenIdCounter;
     uint256 public price = 0.005 ether;
 
-    constructor() ERC721("AINFTs", "AINFT") {}
+    constructor() ERC721("AINFT", "AINFT") {}
 
-    function mint() public payable returns (uint256 newItemId_) {
+    function getAllTokens() public view returns (uint256[] memory) {
+        uint256[] memory tokensList = new uint256[](totalSupply());
+        for (uint256 i = 0; i < totalSupply(); i++) {
+            tokensList[i] = tokenByIndex(i);
+        }
+        return tokensList;
+    }
+
+    function setPrice(uint256 _price) public onlyOwner {
+        price = _price;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint() public payable whenNotPaused {
         require(msg.value >= price, "not enough eth sent");
-        _tokenIds.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+    }
 
-        newItemId_ = _tokenIds.current();
-        _mint(msg.sender, newItemId_);
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
     }
 
     function setTokenURI(uint256 _tokenId, string memory _tokenURI)
         public
         onlyOwner
+        whenNotPaused
     {
         _setTokenURI(_tokenId, _tokenURI);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
