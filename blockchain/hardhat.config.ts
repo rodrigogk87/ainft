459b94a371c15b8a1c0061cd083122e73107c4cd
rodrigogk87 <rodrigogk87@gmail.com>
2023-01-05 17:03:55 -0300

    added dotenv and example

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,22 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "";
-
-// Replace this private key with your Goerli account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = "";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY!]
     }
   }
 };
