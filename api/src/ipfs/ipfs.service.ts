6fca60eec4f41ad7f54a0c34c48c78c3c258ff27
rodrigogk87 <rodrigogk87@gmail.com>
2023-01-06 18:48:47 -0300

    Update ipfs.service.ts

--- api/src/ipfs/ipfs.service.ts
+++ api/src/ipfs/ipfs.service.ts
@@ -1,25 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import { create } from "ipfs-http-client";
+import { create, IPFSHTTPClient } from "ipfs-http-client";
 
+const projectId = process.env.INFURA_PROJECT_ID;
+const projectSecret = process.env.INFURA_API_SECRET;
+const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
 
 @Injectable()
 export class IpfsService {
 
-
     async uploadFileFromUrl(url: string): Promise<string> {
-        const projectId = process.env.INFURA_PROJECT_ID;
-        const projectSecret = process.env.INFURA_API_SECRET;
-        const auth =
-            'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
-
-        const ipfs = await create({
-            host: 'ipfs.infura.io', port: 5001, protocol: 'https',
-            headers: {
-                authorization: auth,
-            },
-        }
-        )
+        const ipfs = await this.getIPFSInstance();
         const response = await axios.get(url, { responseType: 'arraybuffer' })
         const buffer = Buffer.from(response.data, "utf-8")
 
@@ -28,11 +19,12 @@ export class IpfsService {
     }
 
     async uploadFileFromData(data: string): Promise<string> {
-        const projectId = process.env.INFURA_PROJECT_ID;
-        const projectSecret = process.env.INFURA_API_SECRET;
-        const auth =
-            'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+        const ipfs = await this.getIPFSInstance();
+        const { cid } = await ipfs.add(Buffer.from(data))
+        return cid.toString();
+    }
 
+    private async getIPFSInstance(): Promise<IPFSHTTPClient> {
         const ipfs = await create({
             host: 'ipfs.infura.io', port: 5001, protocol: 'https',
             headers: {
@@ -40,8 +32,6 @@ export class IpfsService {
             },
         }
         )
-
-        const { cid } = await ipfs.add(Buffer.from(data))
-        return cid.toString();
+        return ipfs;
     }
 }
