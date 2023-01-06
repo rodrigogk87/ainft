bfd81b0c16211f673b4c259c4826e82d77542111
rodrigogk87 <rodrigogk87@gmail.com>
2023-01-06 18:57:18 -0300

    Update App.js

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,33 +1,13 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import AINFTs from './artifacts/contracts/Ainft.sol/AINFT.json';
-
-const baseURL = "http://localhost:3000";
 
 const NFT_ADDRESS = "0x2b9d6E6e68073E270e19666DF097e8C9beB6Fb07";
 
 function App() {
 
-  let [data, setData] = useState([]);
-  let [image, setImage] = useState(null);
-
-  useEffect(() => {
-
-    //axios.post(baseURL+"/api/generate-images").then((response) => {
-    //});
-  }, [data]);
-
   const requestAccount = async () => {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
-  }
-
-  const onSelect = (e) => {
-    console.log(e);
-    setData({
-      ...data,
-      [e.target.name]: e.target.value,
-    });
   }
 
   const mint = async () => {
@@ -45,47 +25,12 @@ function App() {
 
     console.log("minted", tx);
 
-    /*const burn_transaction = await contract.burn(3);
-    let txb = await burn_transaction.wait();
-    const burn_transaction2 = await contract.burn(4);
-    let txb2 = await burn_transaction2.wait();*/
 
-    /*
-    //preform transaction (get all tokens)
-    const get_all_tokens_transaction = await contract.getAllTokens();
-
-    get_all_tokens_transaction.forEach(async element => {
-      console.log(element.toNumber());
-      //preform transaction (get token uri)
-      const get_token_uri = await contract.tokenURI(element);
-      console.log(get_token_uri);
-    });
-
-    //preform transaction (get owner of token)
-    const get_token_owner_transaction = await contract.ownerOf(0);
-    console.log(get_token_owner_transaction);
-    */
-
-
-    /*
-    data.numberOfImages = 1;
-    axios.post(baseURL + '/openai/generate-images', data)
-      .then(function (response) {
-        //mint here, image should have been stored
-        //setImage(response.data[0]);
-
-      })
-      .catch(function (error) {
-        console.log(error);
-      });*/
   }
 
   return (
     <div className="App">
       AINFTs
-      {image &&
-        <img height={"300px"} width={"300px"} src={image}></img>
-      }
       <button onClick={() => mint()}>Mint</button>
     </div>
   );
